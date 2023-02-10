54df73064bb16ce6101456f6c456e93d7e83cfa6
usuario <usuario@taller-de-santa>
2023-02-10 11:55:03 +0100

    hoal

---
+++ JavaApplication104/src/javaapplication104/calculadora.java
@@ -0,0 +1,31 @@
+package javaapplication104;
+
+/**
+ *
+ * @author usuario
+ */
+public class calculadora {
+    public int op1;
+    public int op2;
+    public int resultado;
+    
+    int suma(int op1, int op2){
+        return this.resultado = op1 + op2;
+    }
+    
+    int resta(int op1, int op2){
+        return this.resultado = op1 - op2;
+    }
+    
+    int multiplicar(int op1, int op2){
+        return this.resultado = op1 * op2;
+    }
+    
+    int dividir(int op1, int op2){
+        return this.resultado = op1 / op2;
+        
+    }
+    
+    
+    
+}
